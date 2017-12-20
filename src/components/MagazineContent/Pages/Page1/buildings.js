dc1ad6ddcd209398f862844ac695791b7335330f
vogelino <contact@vogelino.com>
2017-12-20 22:01:12 +0100

    Generate buildings programatically

--- src/components/MagazineContent/Pages/Page1/buildings.js
+++ src/components/MagazineContent/Pages/Page1/buildings.js
@@ -1,104 +1,57 @@
 const patterns = [
-	'ahoy.jpg',
-	'brijan.gif',
 	'fiesta.jpg',
-	'geometrica.png',
-	'green-goblin.png',
-	'guglieri-speciale.jpg',
 	'junk-mail.jpg',
-	'leather-nunchuck.png',
 	'retro-furnish.png',
-	'science.png',
-	'shattered-island.gif',
-	'wild-sea.png',
 ];
 
 const getRandomPattern = () =>
 	`url('/images/patterns/${patterns[Math.floor(Math.random() * patterns.length)]}')`;
 
-export default [
-	{
-		height: 250,
-		top: 100,
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (((max - min) + 1) + min));
+}
+
+function createBuilding(prevBuildings, index) {
+	const prevBuilding = prevBuildings[index];
+	const height = getRandomInt(50, 300);
+	return [
+		...prevBuildings,
+		{
+			height,
+			top: ((prevBuilding.top + prevBuilding.height) - height),
+			left: prevBuilding.left + 140,
+			zIndex: prevBuilding.zIndex,
+			delay: getRandomInt(0, 2000),
+		},
+	];
+}
+
+const range = (n) => Array.from(Array(n).keys());
+
+const createBuildingsRange = (nBuildings, defaults) =>
+	range(nBuildings).reduce(createBuilding, [{
+		height: 0,
+		top: 0,
 		left: 0,
-		zIndex: 2,
-		delay: 0,
-		rightLetter: 'S',
-	},
-	{
-		height: 200,
-		top: 150,
-		left: 140,
-		zIndex: 2,
-		delay: 100,
-		leftLetter: 't',
-	},
-	{
-		height: 100,
-		top: 300,
-		left: 70,
-		zIndex: 3,
-		delay: 200,
-		topLetter: 'r',
-	},
-	{
-		height: 150,
-		top: 250,
-		left: 210,
-		zIndex: 4,
-		delay: 300,
-		topLetter: 'e',
-	},
-	{
-		height: 100,
-		top: 250,
-		left: 280,
-		zIndex: 2,
-		delay: 400,
-		rightLetter: 'e',
-	},
-	{
-		height: 200,
-		top: 100,
-		left: 350,
-		delay: 500,
-		leftLetter: 'm',
-	},
-	/* END OF MAIN BUILDINGS */
-	{
-		height: 200,
-		top: 400,
-		left: -150,
-		delay: 600,
-	},
-	{
-		height: 150,
-		top: 501,
-		left: -80,
-		delay: 650,
-	},
-	{
-		height: 50,
-		width: 200,
-		top: 672,
-		left: -280,
-		delay: 800,
-	},
-	{
-		height: 160,
-		top: 541,
-		left: -10,
-		delay: 700,
-	},
-	{
-		height: 135,
-		top: 617,
-		left: -80,
-		delay: 750,
-	},
-].map((x) => ({
-	...x,
-	leftBackground: getRandomPattern(),
-	rightBackground: getRandomPattern(),
-	topBackground: getRandomPattern(),
-}));
+		zIndex: 1,
+		...defaults,
+	}]);
+
+export default (() =>
+	range(16)
+		.reduce((prevBuildings, index) => {
+			const top = (index * 49) + 200;
+			const xVariability = index % 2 === 0 ? 70 : 0;
+			const left = xVariability - 200;
+			const newBuildingsRange = createBuildingsRange((13), { top, left });
+			return [...prevBuildings, ...newBuildingsRange];
+		}, [])
+		.filter((b) => b.height !== 0)
+		.filter(() => Math.random() > 0.8)
+		.map((x) => ({
+			...x,
+			leftBackground: getRandomPattern(),
+			rightBackground: getRandomPattern(),
+			topBackground: getRandomPattern(),
+		}))
+)();
--- src/components/MagazineContent/Pages/Page1/index.js
+++ src/components/MagazineContent/Pages/Page1/index.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Building from '../../../Building';
 import buildings from './buildings';
-
-const Buildings = styled.div`
-	width: 560px;
-	height: 420px;
-	position: relative;
-	margin: calc(50vh - 210px) auto;
-`;
 
 class TitleScreen extends Component {
 	constructor(props) {
@@ -25,10 +17,12 @@ class TitleScreen extends Component {
 	}
 	render() {
 		const { ready } = this.state;
-		return (
-			<Buildings>
-				{buildings.map((building) => <Building {...building} ready={ready} />)}
-			</Buildings>
+		return buildings.map((building) => (
+			<Building
+				{...building}
+				key={`${building.top}-${building.left}-${building.height}`}
+				ready={ready}
+			/>)
 		);
 	}
 }
