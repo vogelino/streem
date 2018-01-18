569d7b7ba2cb65633532a7e49ec6176017af0102
vogelino <contact@vogelino.com>
2018-01-18 19:01:22 +0100

    Add Wurstbande buildings

--- src/components/MagazineContent/Pages/Wurstbande/index.js
+++ src/components/MagazineContent/Pages/Wurstbande/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 // import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 import Page from '../Page';
+import Buildings from './Buildings';
 
 const TitleScreen = () => (
 	<Page render={({ ready }) => (
 		<Page.Screen className={ready && 'ready'}>
+			<Buildings />
 		</Page.Screen>
 	)}
 	/>
